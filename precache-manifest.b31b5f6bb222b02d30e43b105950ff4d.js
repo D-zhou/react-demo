2e36ddf4ed618a0aa8c42a7556a61ab6c4c7838f
D_zhou <d_zhou@D-zhoudeMacBook-Pro.local>
2020-05-07 15:57:34 +0800

    Updates

---
+++ precache-manifest.b31b5f6bb222b02d30e43b105950ff4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3389135d91b08360a42874c7c51b3f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e1189e3dc479e95c0ee",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "88f7b9977bd0d9c9d794",
+    "url": "./static/js/2.7b5d802d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7b5d802d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1189e3dc479e95c0ee",
+    "url": "./static/js/main.cc1e3a72.chunk.js"
+  },
+  {
+    "revision": "a2d917653a8a47533497",
+    "url": "./static/js/runtime-main.9b94857b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
